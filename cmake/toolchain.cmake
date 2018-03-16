ba722c482e05d548b2d148aec0bcf41e8de28064
Richard Hodges <richard@trustportal.org>
2018-03-16 11:30:37 +0100

    toolchain script now performs git-submodule-update if polly directory is empty

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,4 +1,14 @@
+
 if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
     set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/polly/cxx17.cmake)
+
+    if (NOT EXISTS ${CMAKE_TOOLCHAIN_FILE})
+        execute_process(WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                COMMAND git submodule init)
+        execute_process(WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                COMMAND git submodule update polly)
+    endif ()
+
     message(STATUS "[project] CMAKE_TOOLCHAIN_FILE: ${CMAKE_TOOLCHAIN_FILE}")
 endif ()
+
